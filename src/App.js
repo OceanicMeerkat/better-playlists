6fd2b6a4c8789add1b32b92f2f421b0aa1433704
OceanicMeerkat <pai.suparanon@gmail.com>
2018-06-13 23:38:42 +0700

    added fake server data to title and aggregate

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
 import React, { Component } from 'react';
 
 import './App.css';
-let test = "test";
+
 let defultTextColor = '#fff';
 let defultStyle ={
   color: defultTextColor
 };
 
-class Aggregate extends Component {
+
+let fakeServerData = {
+  user: {
+    name: 'Paichayon',
+    playlist: [
+      {
+        name: 'Play List A',
+        songs: [{name : 'Song A', duration: 1345}, {name : 'Song B', duration: 1050}, {name : 'Song C', duration: 1250}]
+      },
+      {
+        name: 'Play List B',
+        songs: [{name : 'Song O', duration: 1345}, {name : 'Song P', duration: 1050}, {name : 'Song E', duration: 1250}]
+      },
+      {
+        name: 'Play List E',
+        songs: [{name : 'Song Q', duration: 1345}, {name : 'Song Y', duration: 1050}, {name : 'Song E', duration: 1250}]
+      },
+      {       
+        name: 'Play List O',
+        songs: [{name : 'Song U', duration: 1345}, {name : 'Song D', duration: 1050}, {name : 'Song W', duration: 1250}]
+      }
+    ]
+  }
+}
+
+class PlaylistCounter extends Component {
   render() {
     return (
       <div style={{...defultStyle,width: "40%", display: 'inline-block'}}>
-        <h2>Number Text</h2>
+        <h2>{this.props.playlist.length} playlists</h2>
       </div>
     );
   }
 }
+
+class HoursCounter extends Component {
+  render() {
+
+    let allSongs = this.props.playlist.reduce((songs, playlist) => {
+      return songs.concat(playlist.songs);
+    },[])
+   let totalDuration = allSongs.reduce((sum, song) => {
+      return sum + song.duration
+   }, 0)
+    return (
+      <div style={{...defultStyle,width: "40%", display: 'inline-block'}}>
+        <h2>{Math.round(totalDuration/60)} hours</h2>
+      </div>
+    );
+  }
+}
+
 
 class Filter extends Component {
   render() {
@@ -44,17 +87,33 @@ class Playlist extends Component{
 }
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {serverData:{}}
+  }
+  componentDidMount(){ 
+    setTimeout(() =>{
+    this.setState({serverData : fakeServerData});
+  }, 1000);
+  }
 
   render() {
     return (
       <div className="App">
-      <h1>Title Part</h1>
-        <Aggregate></Aggregate>
-        <Aggregate></Aggregate>
-        <Filter></Filter>
-        <Playlist></Playlist>
-        <Playlist></Playlist>
-        <Playlist></Playlist>
+      { this.state.serverData.user ? <div><h1 style={{...defultStyle, "font-size": "54px"}}>
+        {this.state.serverData.user.name}'s Playlist
+      </h1>
+        
+          <PlaylistCounter playlist={ this.state.serverData.user.playlist}></PlaylistCounter>
+          <HoursCounter playlist={ this.state.serverData.user.playlist}></HoursCounter>
+          
+          
+          <Filter></Filter>
+          <Playlist></Playlist>
+          <Playlist></Playlist>
+          <Playlist></Playlist>
+        </div> : <h1 style={defultStyle}>Loading...</h1>
+      }
       </div>
     );
   }

0f52abdf9f3ab624f1113efcbb779ae6ce2e2dd8
OceanicMeerkat <pai.suparanon@gmail.com>
2018-06-19 23:45:33 +0700

    add href to sign in button

--- src/App.js
+++ src/App.js
@@ -165,7 +165,7 @@ class App extends Component {
      
         
         </div> : <button onClick={()=> 
-          {window.location= window.location.includes('localhost') 
+          {window.location= window.location.href.includes('localhost') 
           ? 'http://localhost:8888/login'
           : 'https://better-playlists-pai.herokuapp.com/login'}
         }
